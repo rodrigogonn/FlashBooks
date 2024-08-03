15d4adf4d23f7050e1c90295b3f00acb45cb3d20
Rodrigo Gonçalves <rodrigo.nascimento@dasa.com.br>
2024-08-03 11:59:40 -0300

    fix mongo connect

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+import { env } from '../environment';
 import { Book } from '../models/book';
 import { User } from '../models/user';
 
 const connect = async () => {
   try {
-    await mongoose.connect('env.MONGODB_URI');
+    await mongoose.connect(env.MONGODB_URI);
     await User.createIndexes();
     await Book.createIndexes();
   } catch {}
