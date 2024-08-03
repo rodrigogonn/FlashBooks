3a6f61db0356ca20ff0c6655a8fdc312808863c3
Rodrigo Gonçalves <rodrigo.nascimento@dasa.com.br>
2024-08-03 11:50:03 -0300

    fix database connection fail

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,13 @@
 import mongoose from 'mongoose';
-import { env } from '../environment';
 import { Book } from '../models/book';
 import { User } from '../models/user';
 
 const connect = async () => {
   try {
-    await mongoose.connect(env.MONGODB_URI);
-    console.log('Conectado ao MongoDB');
-
+    await mongoose.connect('env.MONGODB_URI');
     await User.createIndexes();
     await Book.createIndexes();
-  } catch (error) {
-    console.error('Erro ao conectar ao MongoDB:', error);
-    process.exit(1);
-  }
+  } catch {}
 };
 
 export const database = {
--- src/middlewares/waitForDatabase/index.ts
+++ src/middlewares/waitForDatabase/index.ts
@@ -1,16 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import { connection } from 'mongoose';
+import { connection, ConnectionStates } from 'mongoose';
 
 export const waitForDatabase = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (connection.readyState === 1) {
-    next();
-  } else {
-    connection.once('connected', () => {
+  switch (connection.readyState) {
+    case ConnectionStates.connected: {
       next();
-    });
+      break;
+    }
+    case ConnectionStates.disconnected: {
+      return res.status(503).json({
+        message: 'Service Unavailable: failed to connect to the database',
+      });
+    }
+    default: {
+      connection.once('connected', () => {
+        next();
+      });
+      break;
+    }
   }
 };
