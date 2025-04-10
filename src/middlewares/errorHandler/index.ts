6956d01ddcc666d914268ff233832bb1bc481039
Rodrigo Gonçalves <rodrigo_gonn@hotmail.com>
2025-04-09 22:06:33 -0300

    feat: log in errorHandler

--- src/middlewares/errorHandler/index.ts
+++ src/middlewares/errorHandler/index.ts
@@ -6,6 +6,8 @@ export const errorHandler =
   (fn: RequestHandler): RequestHandler =>
   (req, res: Response<ErrorResponse & { stack: string }>, next) => {
     Promise.resolve(fn(req, res, next)).catch((error) => {
+      console.log('[errorHandler]', error);
+
       res.status(500).json({
         success: false,
         message: env.IS_PRODUCTION ? 'Internal Server Error' : error.message,
